5cf27fb4820174003e2731bf271ff8a8b14e035a
Blake LaPoint <lapoinbe@clarkson.edu>
2021-02-11 11:59:35 -0500

    crud create

---
+++ crud/config/database.php
@@ -0,0 +1,15 @@
+<?php
+$db_name = "grocery";
+$username = "cruduser";
+$password = "this#is#for#lists";
+
+//opens a new mysqli connection (the preferred method today)
+$mysqli = new mysqli("localhost", $dbuser, $dbpass, $dbname);
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("<p class=\"error\">Connect failed: %s</p>", $mysqli->connect_error);
+    exit();
+}
+else {}
+?>
